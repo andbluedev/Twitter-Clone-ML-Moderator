3e3585e4551eb16d3ccf52c1329618666f98e32a
Andrew POURET <andrewhounded92@gmail.com>
2021-04-11 23:04:33 +0200

    removed start_dev

--- spring-microservice/start_dev.sh
+++
@@ -1,6 +0,0 @@
-./mvnw clean
-export REALM_NAME=twitter-clone-ml-project
-export KEYCLOAK_SERVER_URL=https://keycloak.pouretadev.com:8443
-export CLIENT_ID=springboot-microservice-dev
-export CLIENT_SECRET=fa7a6531-020c-4d3a-a652-d2275d39909c
-./mvnw spring-boot:run -Dspring.profiles.active=dev
