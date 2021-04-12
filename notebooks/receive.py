6fb0b009c7701f86ba4ba536dea59eb01c7301c8
Matthieu Jacquand <matthieu.jacquand@gmail.com>
2021-04-12 17:34:22 +0200

    Functionnal send and receive. Make sure module versions are met according to requirements.txt

--- notebooks/receive.py
+++ notebooks/receive.py
@@ -5,6 +5,10 @@
 
 #imports
 
+import time
+
+start_time = time.time()
+
 print("Booting Modbot...\n")
 print("Imports...\n")
 
@@ -21,6 +25,9 @@ from sklearn.pipeline import Pipeline
 from profanity_check import predict as prof_predict
 from profanity_check import predict_prob as prof_predict_prob
 
+
+
+
 print("Setting up text processing...\n")
 
 def text_process(mess):
@@ -36,7 +43,7 @@ y_train = joblib.load('y_train.pkl')
 
 print("Creating pipeline...\n")    
 pipeline = Pipeline([
-   ( 'bow',CountVectorizer(analyzer=text_process)),
+   ( 'bow',CountVectorizer()),
     ('tfidf',TfidfTransformer()),
     ('classifier',model),
 ])    
@@ -54,7 +61,9 @@ def main():
     
    
     
-    
+    end_time = time.time()
+    run_time = end_time-start_time
+    print("Boot completed in : " , run_time, "seconds \n")
     print("Modbot Ready ! \n\n\n")
     
    
@@ -62,19 +71,24 @@ def main():
 
     def callback(ch, method, properties, body):
         
-        sample_spam_predict = pipeline.predict([body])
+        
+        new_body = text_process(str(body))
+        
+        sample_spam_predict = pipeline.predict([str(new_body)])
 
-        sample_prof_predict_prob = prof_predict_prob([body])
+        sample_prof_predict_prob = prof_predict_prob([str(new_body)])
+        
+        my_mod = {'label' : str(sample_spam_predict), 'profanity_score' : float(sample_prof_predict_prob), 'body' : str(new_body) }
         
         print("New message ! Label : ", sample_spam_predict, " / Profanity Index : ", sample_prof_predict_prob, "\n")
-        print("Body: " , body)
+        print("Body: %r" % body, "\n\n\n")
         
+        return my_mod
         
-    
 
     channel.basic_consume(queue='Touitter', on_message_callback=callback, auto_ack=True)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
+    print(' [*] Waiting for messages. To exit press CTRL+C \n ------------------ \n')
     channel.start_consuming()
     
     
